ad58a3ca8c86282bf7f9da35c700b812ebdd630f
Ajith <92452173+rajkumar-hyniva@users.noreply.github.com>
2023-09-20 22:07:14 +0530

    initial4

--- api.js
+++ api.js
@@ -1,21 +1,33 @@
+const multipart = require('aws-lambda-multipart-parser');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = 'your-s3-bucket-name';
+
 const APIGatewayProxyHandler = async (event) => {
-  const { file, fields } = await parseFormData(event);
-  const tags = { filename: file.filename };
   try {
-    await s3Client
+    const formData = multipart.parse(event, true); // Set the second argument to true to parse JSON fields
+
+    // Access the uploaded file and other fields from the formData object
+    const { file, fields } = formData;
+    const tags = { filename: file.filename };
+
+    await s3
       .putObject({
         Bucket: BUCKET_NAME,
         Key: fields.filename || file.filename,
-        Body: file.content,
+        Body: Buffer.from(file.content, 'base64'), // Convert content to a buffer
         Tagging: queryString.encode(tags),
       })
       .promise();
+
     return {
       statusCode: 200,
       body: JSON.stringify({ description: 'file created', result: 'ok' }),
     };
-  } catch (_error) {
-    // this is not ideal error handling, but good enough for the purpose of this example
+  } catch (error) {
+    // Handle errors
+    console.error(error);
     return {
       statusCode: 409,
       body: JSON.stringify({
@@ -26,7 +38,6 @@ const APIGatewayProxyHandler = async (event) => {
   }
 };
 
-
 module.exports = {
-  APIGatewayProxyHandler
+  APIGatewayProxyHandler,
 };
