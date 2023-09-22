f9699dcf5ed23c43b93f268f78f522d5149adda4
Ajith <92452173+rajkumar-hyniva@users.noreply.github.com>
2023-09-22 15:16:05 +0530

    initial9

--- api.js
+++ api.js
@@ -1,69 +1,45 @@
+// 
+
 const AWS = require('aws-sdk');
-const parseMultipart = require('parse-multipart');
 
-const BUCKET = 'serverless-s4-bucket';
-
+const BUCKET = 'serverless-s4-bucket'; // Replace with your S3 bucket name
 const s3 = new AWS.S3();
-
-function extractFile(event) {
-  const contentType = event.headers['Content-Type'];
-  if (!contentType) {
-    throw new Error('Content-Type header is missing in the request.');
-  }
-
-  const boundary = parseMultipart.getBoundary(contentType);
-  if (!boundary) {
-    throw new Error(
-      'Unable to determine the boundary from the Content-Type header.'
-    );
-  }
-
-  const parts = parseMultipart.Parse(
-    Buffer.from(event.body, 'base64'),
-    boundary
-  );
-  console.log('--------parts', parts);
-
-  if (!parts || parts.length === 0) {
-    throw new Error('No parts found in the multipart request.');
-  }
-
-  const [{ filename, data }] = parts;
-
-  if (!filename || !data) {
-    throw new Error(
-      'Invalid or missing filename or data in the multipart request.'
-    );
-  }
-
-  return {
-    filename,
-    data,
-  };
-}
 
 module.exports.handle = async (event) => {
   try {
-    const { filename, data } = extractFile(event);
-    console.log('---------data', data);
-    await s3
-      .putObject({
-        Bucket: BUCKET,
-        Key: filename,
-        Body: data,
-      })
-      .promise();
+    // Extract the filename from the query parameters
+    const { filename } = event.queryStringParameters;
+
+    // Specify the S3 getObject parameters
+    const params = {
+      Bucket: BUCKET,
+      Key: filename,
+    };
+
+    // Retrieve the object from S3
+    const { Body } = await s3.getObject(params).promise();
+
+    // Convert the document to base64 encoding
+    const documentBase64 = Body.toString('base64');
+
+    // Set appropriate response headers
+    const responseHeaders = {
+      'Content-Type': 'application/octet-stream', // Change the content type as needed
+      'Content-Disposition': `attachment; filename="${filename}"`,
+    };
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        link: `https://${BUCKET}.s3.amazonaws.com/${filename}`,
-      }),
+      headers: responseHeaders,
+      isBase64Encoded: true,
+      body: documentBase64,
     };
   } catch (err) {
-    console.log('error-----', err);
+    console.error('Error:', err);
+
+    // Handle errors appropriately
     return {
-      statusCode: 500,
+      statusCode: err.statusCode || 500,
       body: JSON.stringify({ message: err.message }),
     };
   }
