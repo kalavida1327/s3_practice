cffea352752ff0d169c28d2b50f6ceb3aa0c6049
Ajith <92452173+rajkumar-hyniva@users.noreply.github.com>
2023-09-24 23:21:33 +0530

    first commit2

--- api.js
+++ api.js
@@ -1,45 +1,49 @@
-// 
-
 const AWS = require('aws-sdk');
+const parseMultipart = require('parse-multipart');
 
-const BUCKET = 'serverless-s4-bucket'; // Replace with your S3 bucket name
+const BUCKET = 'serverless-s4-bucket';
+
 const s3 = new AWS.S3();
+
+function extractFile(event) {
+  const contentType = event.headers['Content-Type'];
+
+  const boundary = parseMultipart.getBoundary(contentType);
+
+  const parts = parseMultipart.Parse(
+    Buffer.from(event.body, 'base64'),
+    boundary
+  );
+
+  const [{ filename, data }] = parts;
+
+  return {
+    filename,
+    data,
+  };
+}
 
 module.exports.handle = async (event) => {
   try {
-    // Extract the filename from the query parameters
-    const { filename } = event.queryStringParameters;
+    const { filename, data } = extractFile(event);
 
-    // Specify the S3 getObject parameters
-    const params = {
-      Bucket: BUCKET,
-      Key: filename,
-    };
-
-    // Retrieve the object from S3
-    const { Body } = await s3.getObject(params).promise();
-
-    // Convert the document to base64 encoding
-    const documentBase64 = Body.toString('base64');
-
-    // Set appropriate response headers
-    const responseHeaders = {
-      'Content-Type': 'application/octet-stream', // Change the content type as needed
-      'Content-Disposition': `attachment; filename="${filename}"`,
-    };
+    await s3
+      .putObject({
+        Bucket: BUCKET,
+        Key: filename,
+        Body: data,
+      })
+      .promise();
 
     return {
       statusCode: 200,
-      headers: responseHeaders,
-      isBase64Encoded: true,
-      body: documentBase64,
+      body: JSON.stringify({
+        link: `https://${BUCKET}.s3.amazonaws.com/${filename}`,
+      }),
     };
   } catch (err) {
-    console.error('Error:', err);
-
-    // Handle errors appropriately
     return {
-      statusCode: err.statusCode || 500,
+      statusCode: 500,
       body: JSON.stringify({ message: err.message }),
     };
   }
